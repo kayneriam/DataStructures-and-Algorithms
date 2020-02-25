de4305a05245849c70883ec74bfc3c4aaff8ae2b
kayneriam <kayner.iam@gmail.com>
2020-02-25 13:06:46 +0300

    Implemented Static and Dynamic arrays

---
+++ data-structures/dynamic_array.js
@@ -0,0 +1,28 @@
+let StaticArray = require('./static_array');
+
+class DynamicArray extends StaticArray {
+  constructor() {
+    super(1000);
+  }
+
+  get(index) {
+    if (index > this.size - 1) {
+      this.resize(index - this.size + 1);
+    }
+    super.get(index);
+  }
+
+  set(index, value) {
+    if (index > this.size - 1) {
+      this.resize(index - this.size + 1);
+    }
+    super.set(index, value);
+  }
+
+  resize(newSize) {
+    this.body = new Array(...this.body);
+    this.size += newSize;
+  }
+}
+
+module.exports = DynamicArray;
